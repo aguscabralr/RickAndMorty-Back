c784c270c8b9f53e5becfc4f946ccb7369cea938
Agustin Ignacio Cabral Robledo <acabralrobledo@gmail.com>
2023-08-01 23:47:39 -0300

    Update deploy DB

--- src/db.js
+++ src/db.js
@@ -4,7 +4,16 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_DEPLOY } = process.env;
 const { usersMd, favoritesMd } = require('./models/index');
 
 const database = new Sequelize(DB_DEPLOY,
-   { logging: false, native: false }
+   {
+      logging: false,
+      native: false,
+      dialectOptions: {
+         ssl: {
+            require: true,
+            rejectUnauthorized: false
+         }
+      }
+   }
 );
 
 // const database = new Sequelize(`postgres:${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
